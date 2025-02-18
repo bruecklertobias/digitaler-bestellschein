41b5ea3d9c1d06e0e7be72fccd6a9f8d609fed67
bruecklertobias <43436906+bruecklertobias@users.noreply.github.com>
2025-02-18 21:52:10 +0100

    Update next.config.mjs
    fix styling issue

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,19 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:;"
+          },
+        ],
+      },
+    ];
+  },
 }
 
 mergeConfig(nextConfig, userConfig)
